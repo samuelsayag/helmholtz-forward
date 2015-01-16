0a4c9e47558f5d91880247bfc995fed97175454d
Samuel ROUAH-SAYAG <samuelsayag@gmail.com>
2015-01-16 11:42:27 +0200

    graphic in simu

--- matlab/Exact_Finite_Difference_Schemes_for_Solving_Helmholtz_Equation_at_any_Wavenumber/one-dimensional/article_simu.m
+++ matlab/Exact_Finite_Difference_Schemes_for_Solving_Helmholtz_Equation_at_any_Wavenumber/one-dimensional/article_simu.m
@@ -40,13 +40,42 @@ sols = [sols,sol];
 params = [params, param];
 
 error = cell(size(sols));
-
+analytic = cell(size(sols));
 
 for i = 1:size(error,1)
     for j = 1:size(error,2)
         k = params{i,j}.k;
         x = linspace(0,1, params{i,j}.m );
-        analytic = analytic_sol_1D(k, x);
-        error{i,j} = norm((analytic - sols{i,j}), Inf );
+        analytic{i,j} = analytic_sol_1D(k, x);
+        error{i,j} = norm((analytic{i,j} - sols{i,j}), Inf );
     end    
 end
+
+i = 2;
+figure % create new figure
+subplot(2,3,1) % first subplot
+plot(analytic{i,1});
+title('Analytic sol')
+axis equal
+
+subplot(2,3,2) % first subplot
+plot(sols{i,1});
+title('SBC-SFD')
+axis equal
+
+subplot(2,3,3) % first subplot
+plot(sols{i,2});
+title('SBC-NFD')
+axis equal
+
+subplot(2,3,4) % first subplot
+plot(sols{i,3});
+title('NBC-SFD')
+axis equal
+
+subplot(2,3,5) % first subplot
+plot(sols{i,4});
+title('NBC-NFD')
+axis equal
+
+
