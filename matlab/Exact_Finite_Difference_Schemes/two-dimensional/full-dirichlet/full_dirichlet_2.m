3c8a0d112f657295c1b8a8cb846b299cc9496e76
samouille666 <samouille666@hotmail.com>
2015-03-03 19:25:32 +0200

    fix stuffs

---
+++ matlab/Exact_Finite_Difference_Schemes/two-dimensional/full-dirichlet/full_dirichlet_2.m
@@ -0,0 +1,86 @@
+%==========================================================================
+% replay the simulation of the article that originated the new schemes for
+% 1D and 2D problem.
+% These simulations are for the 2D problem
+%==========================================================================
+close all; clear all; clc;
+addpath(genpath('..\..\..\matlab'));
+
+% generic parameters of the simulations
+sim_param.h = [0.02];
+% k = [25];
+% k = [150, 100, 70];
+% k = sqrt(2)* [30, 25, 20];
+k = sqrt(2)* [30, 25, 20, 15, 10, 5];
+d_k = size(k,2);
+
+
+sim_param.a = 0;
+sim_param.b = 1;
+sim_param.d = 1;
+sim_param.c = 0;
+sim_param.m = (sim_param.b - sim_param.a)./sim_param.h;
+sim_param.n = (sim_param.d - sim_param.c)./sim_param.h;
+
+sim_param.theta = pi/4;
+
+% dirichlet boundary
+% parameters necessary to compute boundary points
+sim_param.dirichlet.S = @(params, A, b, i, j) analytic_sol_2D(params.k,... 
+    params.theta, i * params.h, (j-1) * params.h);
+sim_param.dirichlet.W = @(params, A, b, i, j) analytic_sol_2D(params.k,... 
+    params.theta, (i-1) * params.h, j * params.h);
+sim_param.dirichlet.N = @(params, A, b, i, j) analytic_sol_2D(params.k,... 
+    params.theta, i * params.h, (j+1) * params.h);
+sim_param.dirichlet.E = @(params, A, b, i, j) analytic_sol_2D(params.k,... 
+    params.theta, (i+1) * params.h, j * params.h);
+
+% declaration of solution structures
+sols = {};
+params = {};
+
+% simu interior std
+sim_param.interior = 'std';
+[ sol, param ] = simulation_k_2D( k, sim_param );
+sols = [sols, sol];
+params = [params, param];
+
+% simu interior NEW
+sim_param.interior = 'new';
+sim_param.bessel = @(x) bessel_integral(x, 0, pi/2);
+[ sol, param ] = simulation_k_2D( k, sim_param );
+sols = [sols, sol];
+params = [params, param];
+
+% simu interior NEW
+sim_param.interior = 'new';
+sim_param.bessel = @(x) bessel_exact_theta(x, sim_param.theta);
+[ sol, param ] = simulation_k_2D( k, sim_param );
+sols = [sols, sol];
+params = [params, param];
+
+% prepare the meshgrid to calculate the analytic solution or to propose
+% graphical representation of the solutions
+c_k = mat2cell(k', ones(1, d_k));
+x = linspace(1, sim_param.m, sim_param.m) * sim_param.h;
+y = linspace(sim_param.n, 1, sim_param.n) * sim_param.h;
+[X,Y] = meshgrid(x,y);
+
+% calculate the error for each simulation
+error = cell(size(sols));
+analytic = cell(size(sols));
+fa = @(t) analytic_sol_2D(t, sim_param.theta, X, Y);
+fe = @(a, s) norm((a - s), Inf );
+for j = 1:size(error,2)
+    analytic(:,j) = cellfun(fa, c_k, 'UniformOutput', false);
+    error(:,j) = cellfun( fe, analytic(:,j), sols(:,j), 'UniformOutput', false );
+end
+
+% RESULT FOR STANDARD AND NEW ('std', 'new')
+% just the central scheme
+title1 = {'' 'error' 'error' 'error'};
+title2 = { 'k' 'SFD' 'NFD - J0(kh)' 'NFD - exact theta'};
+res_tab = [title1;title2];
+res_tab = [res_tab; c_k error ];
+res_tab
+
---
+++ matlab/Exact_Finite_Difference_Schemes/two-dimensional/simulation_intg_theta_2D.m
@@ -0,0 +1,50 @@
+function [ sols, params, As, Bs ] = simulation_intg_theta_2D( theta, sim_param )
+%SIMULATION_K_H
+% perform a simulation for a vector k and h and some fixed parameters
+% given by sim_param
+% return solution cell vector
+
+% simultation output structures declaration
+dtheta = size(theta,2);
+
+sols = cell(dtheta, 1);
+params = cell(dtheta, 1);
+
+As = cell(dtheta, 1);
+Bs = cell(dtheta, 1);
+
+cpt_sol = 1;
+fourk = 1e-2; 
+sim_param
+for j = 1:dtheta
+    % prepare temporary parameters
+    sim_param.theta = theta(j);
+    sim_param.bessel = @(x) bessel_integral(x,...
+        0, pi/2) ;    
+
+%     sim_param.bessel = @(x) bessel_integral(x,...
+%         theta(1,j) - fourk,  + fourk) ;
+
+%     sim_param.bessel = @(x) bessel_exact_theta(x, theta(1,j));
+
+    
+    % build the simulation function
+    [ func_scheme, sim_param ] = helmholtz_2D_scheme_factory( sim_param );
+    % generate the matrix and vector of the problem
+    [A, b] = build_two_dimensional_problem2(sim_param, func_scheme);
+    As{cpt_sol, 1} = A;
+    Bs{cpt_sol, 1} = b;
+    % compute the solution of the equation
+    tmp_sol = A\b;    
+%     tmp_sol = bicgstab(A, b, 1e-6, 1e5);
+%       tmp_sol = qmr(A, b, 1e-6, 1e5);
+%     tmp_sol = gmres(A, b, 60, 1e-6, 300);
+    tmp_sol = transpose(reshape(tmp_sol, sim_param.m, sim_param.n));
+    sols{cpt_sol, 1} = tmp_sol;
+    params{cpt_sol, 1} = sim_param;
+    cpt_sol = cpt_sol + 1;
+end
+
+
+end
+
